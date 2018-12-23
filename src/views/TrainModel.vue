8262e7be38ba08fc0b4d1b529f2c3e1bee5155ea
Beta Priyoko <beta@softwareseni.com>
2018-12-23 13:09:24 +0700

    update frontend

--- src/views/TrainModel.vue
+++ src/views/TrainModel.vue
@@ -2,31 +2,87 @@
   <v-container grid-list-md>
     <!-- <h1>Train Model</h1> -->
     <v-layout row wrap>
-      <v-flex xs6 md6 lg6>
-        <h2 class="mb-3">Dataset Spam</h2>
-        <el-upload
-          class="upload-demo"
-          drag
-          action="localhost"
-          :on-remove="handleRemove"
-          :auto-upload="false"
-          :on-change="handlePreview">
-          <i class="el-icon-upload"></i>
-          <div class="el-upload__text">Drop file here or <em>click to upload</em></div>
-        </el-upload>
+      <v-flex row wrap xs12 md12 lg12>
+        <v-alert
+          v-model="alertSuccess"
+          class="green lighten-1"
+          type="success"
+        >
+          Dataset Berhasil Diupload
+        </v-alert>
+        <v-alert
+          v-model="alertError"
+          class="red lighten-1"
+          type="error"
+        >
+          Dataset Gagal DiUpload
+        </v-alert>
+        <v-alert
+          v-model="alertTraining"
+          class="green lighten-1"
+          type="success"
+        >
+          {{alertTrainingMessage}}
+        </v-alert>
       </v-flex>
       <v-flex xs6 md6 lg6>
+        <h2 class="mb-3">Dataset Spam</h2>
+        <v-text-field 
+          label="Select File" 
+          @click='pickFile' 
+          v-model='fileSpamName' 
+          prepend-icon='attach_file'
+          :disabled="btnSpamDisable ? true:false"></v-text-field>
+				<form>
+          <input
+						type="file"
+						style="display: none"
+						ref="spam"
+            name="spam"
+						accept="json/*"
+						@change="onFilePickedSpam"
+					>
+          <v-btn 
+            color="blue-grey darken-2"
+            class="white--text mt-1"
+            text-xs-center
+            block
+            v-on:click.prevent="handleFileSpam"
+            :disabled="btnSpamDisable ? true:false"
+          >
+            Upload Dataset
+          </v-btn>
+        </form>
+      </v-flex>
+
+      <v-flex xs6 md6 lg6>
         <h2 class="mb-3">Dataset Not Spam</h2>
-        <el-upload
-          class="upload-demo"
-          drag
-          action="localhost"
-          :on-remove="handleRemove2"
-          :auto-upload="false"
-          :on-change="handlePreview2">
-          <i class="el-icon-upload"></i>
-          <div class="el-upload__text">Drop file here or <em>click to upload</em></div>
-        </el-upload>
+        <v-text-field 
+        label="Select File" 
+        @click='pickFileNotSpam' 
+        v-model='fileNotSpamName' 
+        prepend-icon='attach_file'
+        :disabled="btnNotSpamDisable ? true:false"></v-text-field>
+				<form>
+          <input
+						type="file"
+						style="display: none"
+						ref="notspam"
+            name="notspam"
+						accept="json/*"
+						@change="onFilePickedNotSpam"
+					>
+          <v-btn 
+            color="blue-grey darken-2"
+            class="white--text mt-1"
+            text-xs-center
+            block
+            v-on:click.prevent="handleFileNotSpam"
+            :disabled="btnNotSpamDisable ? true:false"
+          >
+            Upload Dataset
+          </v-btn>
+        </form>
       </v-flex>
     </v-layout>
 
@@ -38,7 +94,7 @@
           @click="handleTrain"
           text-xs-center
           block
-          :disabled="valid ? false : true"
+          :disabled="btnSpamDisable && btnNotSpamDisable ? false:true"
         >
           Latih Model
         </v-btn>
@@ -48,52 +104,112 @@
 </template>
 
 <script>
+import axiosFu from 'axios-fileupload'
 import axios from 'axios'
 export default {
   name: 'TrainModel',
   data(){
     return{
-      fileSpam:[],
-      fileNotSpam: [],
-      valid: false
-    }
-  },
-  watch:{
-    fileSpam: function(value) {
-      if(this.fileSpam.length > 0 && this.fileNotSpam.length >0){
-        this.valid = true
-      }else{
-        this.valid = false
-      }
-    },
-
-    fileNotSpam: function(value) {
-      if(this.fileSpam.length > 0 && this.fileNotSpam.length >0){
-        this.valid = true
-      }else{
-        this.valid = false
-      }
+      alertTraining: false,
+      alertTrainingMessage: '',
+      alertError: false,
+      alertSuccess: false,
+      btnSpamDisable: false,
+      btnNotSpamDisable:false,
+      valid: false,
+      fileSpamName: '',
+      fileSpam: '',
+      imageUrl: '',
+      fileNotSpamName: '',
+      fileNotSpam: '',
+      isLoading: false
     }
   },
   methods:{
-    handlePreview(file){
-      this.fileSpam.push(file)
+    pickFile() {
+      this.$refs.spam.click()
     },
 
-    handlePreview2(file){
-      this.fileNotSpam.push(file)
+    pickFileNotSpam(){
+      this.$refs.notspam.click()
     },
 
-    handleRemove(file){
-      this.fileSpam = []
+    onFilePickedSpam(e) {
+			const files = e.target.files
+			if(files[0] !== undefined) {
+				this.fileSpamName = files[0].name
+				if(this.fileSpamName.lastIndexOf('.') <= 0) {
+					return
+				}
+				const fr = new FileReader ()
+				fr.readAsDataURL(files[0])
+				fr.addEventListener('load', () => {
+          this.fileSpam = files[0]
+				})
+      }
     },
 
-    handleRemove2(file){
-      this.fileNotSpam = []
+    onFilePickedNotSpam(e){
+      const files = e.target.files
+			if(files[0] !== undefined) {
+				this.fileNotSpamName = files[0].name
+				if(this.fileNotSpamName.lastIndexOf('.') <= 0) {
+					return
+				}
+				const fr = new FileReader ()
+				fr.readAsDataURL(files[0])
+				fr.addEventListener('load', () => {
+          this.fileNotSpam = files[0]
+				})
+      }
+    },
+    
+    handleFileSpam(){
+      axiosFu('http://localhost:3000/datasets',this.fileSpam)
+      .then(res => {
+        if(res.data !== 'OK'){
+          this.alertError = true
+          setTimeout(() => {
+            this.alertError = false
+          },5000)
+        }else{
+          this.alertSuccess = true
+          setTimeout(() => {
+            this.alertSuccess = false
+          },5000)
+          this.btnSpamDisable = true
+        }
+      })
+    },
+
+    handleFileNotSpam(){
+      axiosFu('http://localhost:3000/datasets',this.fileNotSpam)
+      .then(res => {
+        if(res.data !== 'OK'){
+          this.alertError = true
+          setTimeout(() => {
+            this.alertError = false
+          },5000)
+        }else{
+          this.alertSuccess = true
+          setTimeout(() => {
+            this.alertSuccess = false
+          },5000)
+          this.btnNotSpamDisable = true
+        }
+      })
     },
 
     handleTrain(){
-
+      axios.post('http://localhost:3000/train')
+      .then(res => {
+        if(res.data.error == false){
+          this.alertTraining = true
+          this.alertTrainingMessage = res.data.message
+        }else{
+          this.alertTraining = false
+        }
+      })
     }
   }
 }
